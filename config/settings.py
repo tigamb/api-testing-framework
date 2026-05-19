fd18428e74551ca2eb00da9a001ea3eb675fb898
Danny <tigaboa@gmail.com>
2026-05-19 21:33:19 +0300

    add .env support with python-dotenv

--- config/settings.py
+++ config/settings.py
@@ -1,10 +1,13 @@
-# config/settings.py
 
-BASE_URL = "https://jsonplaceholder.typicode.com"
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+BASE_URL = os.getenv("BASE_URL", "https://jsonplaceholder.typicode.com")
+TIMEOUT = int(os.getenv("TIMEOUT","10"))
 
 HEADERS = {
     "Content-Type": "application/json",
     "Accept": "application/json"
 }
-
-TIMEOUT = 10
